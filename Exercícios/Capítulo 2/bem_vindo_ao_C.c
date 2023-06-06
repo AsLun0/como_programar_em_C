f0a3291f11bfb585293e709ed021c9dc4f2a66a3
Paulo Lunas <paulovictor.lunas@gmail.com>
2023-06-06 00:11:42 -0400

    versao 1

---
+++ Exercícios/Capítulo 2/bem_vindo_ao_C.c
@@ -0,0 +1,16 @@
+/* Primeiro programa em C */
+main(){
+    printf("Bem-vindo ao C!\n");
+}
+
+/* Imprimindo em uma linha com duas instruções printf */
+main(){
+    printf ("Bem-vindo");
+    printf("ao C!\n");
+
+}
+
+/* Imprimindo várias linhas com um único printf */
+main(){
+    printf("Bem-vindo \nao\nC\n");
+}
