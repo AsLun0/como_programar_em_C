4dae35d52667dca134d68acc8c37cc88f9be65d1
Paulo Lunas <paulovictor.lunas@gmail.com>
2023-06-07 00:23:16 -0400

    att

--- Exercícios/Capítulo 2/programa_de_soma.c
+++ Exercícios/Capítulo 2/programa_de_soma.c
@@ -1,7 +1,21 @@
-/* 2.3 Outro Programa Simples em C: Somar Dois Números 
-Inteiros */
+/* 2.3 Outro Programa Simples em C: Somar Dois Números Inteiros */
 
 /* Programa de soma */
 
 #include <stdio.h>
 
+main(){
+    int inteiro1, inteiro2, soma; // declaração das variáveis
+    
+    printf("Entre com o primeiro inteiro\n"); // prompt
+    scanf("%d", &inteiro1); // lê um inteiro
+    
+    printf("Entre com o segundo inteiro\n"); // prompt
+    scanf("%d", &inteiro2); // lê um inteiro
+
+    soma = inteiro1 + inteiro2; // atribui soma
+
+    printf("A soma é %d\n",soma); // imprime soma
+
+    return 0; // indica que o programa foi bem-sucedido 
+}
