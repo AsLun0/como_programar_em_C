b72ab83ff18e09c4306d2e603041bbc1d44117ab
Paulo Lunas <paulovictor.lunas@gmail.com>
2023-06-09 12:47:54 -0400

    att

--- Exercícios/Capítulo 2/1_bem_vindo_ao_C.c
+++ Exercícios/Capítulo 2/1_bem_vindo_ao_C.c
@@ -4,16 +4,21 @@
 
 main(){
     printf("Bem-vindo ao C!\n");
-}
+
+    return 0;
+    }
 
 /* Imprimindo em uma linha com duas instruções printf */
 main(){
     printf ("Bem-vindo");
     printf("ao C!\n");
 
+    return 0;
 }
 
 /* Imprimindo várias linhas com um único printf */
 main(){
     printf("Bem-vindo \nao\nC\n");
+
+    return 0;
 }
---
+++ Exercícios/Capítulo 2/5_exerc_2_ponto_4.c
@@ -0,0 +1,25 @@
+/* 2.4 Escreva uma instrução (ou comentário) para realizar cada um dos pedidos seguintes:
+a) Crie um comentário declarando que um programa calculará o produto de três números 
+inteiros.
+b) Declare as variáveis x, y, z e resultado como sendo do tipo int.
+c) Peça ao usuário para digitar três números inteiros.
+d) Leia os três números inteiros fornecidos através do teclado e armazene-os nas variáveis x, 
+y e z.
+e) Calcule o produto dos três números inteiros contidos nas variáveis x, y e z e atribua o 
+resultado à variável resultado.
+f) Imprima "O produto e" seguido do valor da variável resultado */
+
+#include <stdio.h>
+
+main(){
+    int x, y, z, resultado; // questão B
+
+    printf("Entre com tres numeros inteiros: "); // questão C
+    scanf("%d%d%d",&x,&y,&z); // questão D
+
+    resultado = x * y * z; //questão E
+
+    printf("O produto eh %d\n", resultado);
+
+    return 0;
+}
